f50ff32a73d8af44a3d0c151452bb180c7ad28d7
Louis Chiero <louis.chiero@outlook.com>
2016-05-01 14:54:00 -0500

    Add Deployment Files
    Add files that will let Azure know which project deploys to which site.

---
+++ deploy.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+IF "%DEPLOY_PROJ%" == "Angular" (
+	deploy.Angular.cmd
+) ELSE (
+	IF "%DEPLOY_PROJ%" == "API" (
+		deploy.API.cmd
+	) ELSE (
+		echo You have to set DEPLOY_PROJ setting to either "Angular" or "API"
+		exit /b 1
+	)
+)
